868d45c036571d482e0a031c1910e6f1fa1faa30
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-27 01:51:24 -0400

    Add Socket-io and Cors

--- models/server.js
+++ models/server.js
@@ -1,6 +1,10 @@
 // Server Express
 const express = require("express");
 const http = require("http");
+const socketio = require("socket.io");
+const cors = require("socket.io");
+
+const Sockets = require("./sockes");
 
 class Server {
   constructor() {
@@ -8,9 +12,39 @@ class Server {
 
     // Http
     this.server = http.createServer(this.app);
+
+    // Configurmos socket
+    this.io = socketio(this.server, {
+      // config
+    });
+  }
+
+  middlewares() {
+    // Directorio public
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    this.app.use(
+      cors({
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: "*",
+        credentials: true,
+      })
+    );
+  }
+
+  configSockets() {
+    new Sockets(this.io);
   }
 
   execute() {
+    // Inicializamos los middlewares
+    this.middlewares();
+
+    // Inicializamos los sockets
+    this.configSockets();
+
+    // Inicializamos el server
     this.server.listen(this.port, () => {
       console.log("Server corriendo en puerto:", this.port);
     });
