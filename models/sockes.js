fe20b86e4327b6c9c9447585323ff7088e4dd9d6
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-27 05:30:51 -0400

    Add functions create party and join party

--- models/sockes.js
+++ models/sockes.js
@@ -1,3 +1,5 @@
+const { generateToken } = require("../utils/jwt");
+
 class Sockets {
   constructor(io) {
     this.io = io;
@@ -7,14 +9,46 @@ class Sockets {
   socketEvents() {
     // escuchando event
     this.io.on("connection", (socket) => {
-      socket.on("msg-client", (data) => {
-        console.log(data);
+      console.log("player connected");
 
-        // emitiendo
-        this.io.emit("msg-server", data);
+      // Crear una sala
+      socket.on("crear-partida", async () => {
+        try {
+          // Generamos el token
+          const token = await generateToken();
+
+          // Emitimos el token
+          socket.emit("token-generado", token);
+
+          //Unimos al jugador que creo la partida a la sala
+          socket.join(token);
+
+          // Contar el número de sockets en la sala ||DEV
+          const roomInfo = this.io.sockets.adapter.rooms.get(token);
+          const numPlayers = roomInfo ? roomInfo.size : 0;
+
+          console.log(`Hay ${numPlayers} jugadores en la sala ${token}`);
+        } catch (err) {
+          console.log("Error al generar el token:", err);
+        }
+      });
+
+      //   Unirse a una sala
+      socket.on("unirse-partida", (code) => {
+        socket.join(code);
+        const roomInfo = this.io.sockets.adapter.rooms.get(code);
+
+        // Contar el número de sockets en la sala
+        const numPlayers = roomInfo ? roomInfo.size : 0;
+
+        console.log(`Hay ${numPlayers} jugadores en la sala ${code}`);
+      });
+
+      socket.on("disconnect", () => {
+        this.io.emit(console.log("player desconnected"));
       });
     });
   }
 }
 
-module.exports = Sockets
+module.exports = Sockets;
