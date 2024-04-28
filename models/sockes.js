83b089a5ee760372b4865d62f6cb7bc8e669c689
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-28 11:28:08 -0400

    Add moves and rollback logic

--- models/sockes.js
+++ models/sockes.js
@@ -12,11 +12,15 @@ class Sockets {
       console.log("player connected");
 
       // Crear una sala
-      socket.on("crear-partida", async () => {
+      socket.on("crear-partida", async (id) => {
         try {
           // Generamos el token
           const token = await generateToken();
-
+          this.game.set(token, {
+            players: [id],
+            currentPlayer: id,
+            squares: Array(9).fill(null),
+          });
           // Emitimos el token
           socket.emit("token-generado", token);
 
@@ -34,12 +38,77 @@ class Sockets {
       });
 
       //   Unirse a una sala
-      socket.on("unirse-partida", (code) => {
-        socket.join(code);
-        const roomInfo = this.io.sockets.adapter.rooms.get(code);
+      socket.on("unirse-partida", ({ codeRoom, id }) => {
+        const game = this.game.get(codeRoom);
+        // Codigo incorrecto
+        if (!game) {
+          console.log("La sala no existe");
+          return;
+        }
+        // Validar max players
+        if (game.players.length >= 2)
+          return console.log("Numero maximo de jugadores");
 
-        // Contar el número de sockets en la sala
+        socket.join(codeRoom);
+        game.players.push(id);
+        const players = game.players;
+        this.io.to(codeRoom).emit("jugador-unido", { players, codeRoom });
+
+        const roomInfo = this.io.sockets.adapter.rooms.get(codeRoom);
         const numPlayers = roomInfo ? roomInfo.size : 0;
+        console.log(`Hay ${numPlayers} jugadores en la sala ${game}`);
+      });
+
+      // Movimiento del jugador
+      socket.on("jugada", (data) => {
+        const { code, squares, currentPlayer } = data;
+        const game = this.game.get(code);
+
+        if (!game || !game.players.includes(currentPlayer)) {
+          console.log("Error: Jugador o juego no encontrado");
+          return;
+        }
+        const currentPlayerIndex = game.players.indexOf(currentPlayer);
+        const nextPlayerIndex = (currentPlayerIndex + 1) % game.players.length;
+        game.currentPlayer = game.players[nextPlayerIndex];
+
+        this.io.to(code).emit("jugada", {
+          squares: squares,
+          currentPlayer: game.currentPlayer,
+        });
+      });
+      // Revancha
+      socket.on("revancha", ({ playerId, token }) => {
+        const game = this.game.get(token);
+        if (!game || !game.players.includes(playerId)) {
+          console.log("Error: Jugador o juego no encontrado");
+          return;
+        }
+        const opponentId = game.players.find((player) => player !== playerId);
+        this.io.to(opponentId).emit("solicitud-revancha");
+      });
+
+      socket.on("respuesta-revancha", ({ playerId, token, accepted }) => {
+        const game = this.game.get(token);
+        const reciverID = game.players.find((player) => player !== playerId);
+
+        // Emitimos si la petición fue rechazada
+        this.io.to(reciverID).emit("respuesta-revancha", accepted);
+
+        if (accepted) {
+          // Si la revancha es aceptada, reiniciar el juego
+          setTimeout(() => {
+            game.squares = Array(9).fill(null);
+            game.currentPlayer = game.players[0];
+            this.io.to(token).emit("reinicio", {
+              squares: game.squares,
+              players: game.players,
+            });
+          }, 1500);
+        } else {
+          this.io.to(token).emit("abandono-partida");
+        }
+      });
 
         console.log(`Hay ${numPlayers} jugadores en la sala ${code}`);
       });
